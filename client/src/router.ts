e7671589fa078aa205cb23632f83ab6a9fccf1d2
alan <hsymyth@gmail.com>
2025-07-09 22:40:57 +0900

    class3

--- client/src/router.ts
+++ client/src/router.ts
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import RoomList from './views/RoomList.vue'
+import ChatRoom from "./views/ChatRoom.vue";
 
 export const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', component: RoomList },
+        { path: '/room/:name', component: ChatRoom }
     ]
 })
